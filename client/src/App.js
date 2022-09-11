349506c8e6b38165957ada0dc93a9f03c42561b7
Andy <349562769@gapps.yrdsb.ca>
2022-09-11 15:07:29 -0400

    upload

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import styled from 'styled-components';
 import LandingPage from './LandingPage';
 import Nav from './Nav';
 import Menu from './Menu';
+import Upload from './Upload';
 function App() {
 
   return (
@@ -14,6 +15,7 @@ function App() {
             <Routes>
                  <Route exact path='/' element={<LandingPage/>}></Route>
                  <Route exact path='/menu' element={<Menu/>}></Route>
+                 <Route exact path='/upload' element={<Upload/>}></Route>
           </Routes>
         </div>
       </Router>
