06762c006e18878804c322d98559f2a034b917de
Orion <orion.chen@hotmail.com>
2022-09-11 16:27:29 -0400

    fixed kanaads goof

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import styled from 'styled-components';
 import LandingPage from './LandingPage';
 import Nav from './Nav';
 import Menu from './Menu';
+import Upload from './Upload';
 
 function App() {
 
@@ -13,7 +14,9 @@ function App() {
     <Router>
         <div className='App'>
             <Routes>
-                 <Route exact path='/' element={<Menu/>}></Route>
+                 <Route exact path='/' element={<LandingPage/>}></Route>
+                 <Route exact path='/menu' element={<Menu/>}></Route>
+                 <Route exact path='/upload' element={<Upload/>}></Route>
           </Routes>
         </div>
       </Router>
