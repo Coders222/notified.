9335010245994bf0a2fa75431a58ba35ee053765
Andy <349562769@gapps.yrdsb.ca>
2022-09-11 10:24:04 -0400

    get started working

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import React from 'react';
 import styled from 'styled-components'; 
 import LandingPage from './LandingPage';
 import Nav from './Nav';
-
+import Menu from './Menu';
 function App() {
 
   return (
@@ -13,6 +13,7 @@ function App() {
         <div className='App'>
             <Routes>
                  <Route exact path='/' element={<LandingPage/>}></Route>
+                 <Route exact path='/menu' element={<Menu/>}></Route>
           </Routes>
         </div>
       </Router>
