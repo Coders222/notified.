26d4586e622c5e774a47e3a0cd39f6fe03738468
kanaad222 <kanaadizawesome0@gmail.com>
2022-09-11 15:57:06 -0400

    dingus

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import React from 'react';
 import styled from 'styled-components'; 
 import LandingPage from './LandingPage';
 import Nav from './Nav';
+import Menu from './Menu';
 
 function App() {
 
@@ -12,7 +13,7 @@ function App() {
     <Router>
         <div className='App'>
             <Routes>
-                 <Route exact path='/' element={<LandingPage/>}></Route>
+                 <Route exact path='/' element={<Menu/>}></Route>
           </Routes>
         </div>
       </Router>
--- client/src/Menu.js
+++ client/src/Menu.js
@@ -9,7 +9,7 @@ function Images(){
     return(
         <div>
             <img style={{ width: 97, height: 108 }} src={img2} alt="Math Logo"/>
-            <img style={{ width: 130, height: 117 }} src={img3} alt="Science Logo"/>
+            <img style={{ width: 120, height: 117 }} src={img3} alt="Science Logo"/>
             <img style={{ width: 121, height: 117 }} src={img1} alt="Accounting Logo"/>
         </div>
     )
