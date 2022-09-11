6d6bcb31ac3d7615b9237f0fd6d7f61239c29e5d
Andy <349562769@gapps.yrdsb.ca>
2022-09-11 01:03:35 -0400

    nav start

---
+++ client/src/Nav.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components'; 
+import logo from './images/logo.png';
+function Nav() {
+    const Container = styled.div`
+        width: 99vw;
+        height: 10vw;
+        
+    `
+    const Logo = styled.div`
+        width:5vw;
+        height:5vw;
+        background-image: url(${bg});
+        background-repeat: no-repeat;
+        background-position: center;
+        background-size: cover;
+    `
+    return (
+        <Container>
+
+        </Container>
+    );
+  }
+  
+  export default App;
