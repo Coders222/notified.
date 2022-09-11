d42fa0536a62c5d74303d7fc6e1ca2c5d8aa6e6c
Orion <orion.chen@hotmail.com>
2022-09-11 00:19:18 -0400

    landing

--- client/src/landingPage.js
+++ client/src/landingPage.js
@@ -1,5 +1,6 @@
 import './App.css';
 import logo from './images/logo.png';
+import bg from './images/frontBackground.png'
 import React from 'react';
 import styled from 'styled-components'; 
 
@@ -7,22 +8,48 @@ import styled from 'styled-components';
 function LandingPage() {
 
   const Container = styled.div`
-    width: 100%;
-    height: 100%;
-    display: flex;
-    justify-content: center;
-
+    width: 99vw;
+    height: 50vw;
+    
   `
 
+  const Header = styled.div`
+    height: 7vw;
+    width: 99vw;
+    position: relative;
+    display: flex;
+  `
   const Logo = styled.div`
-    float:right;
-    background-image: ${logo};
-    width:2vw;
-    height: 3.5vh;
+
+    height: 7vw;
+    width: 21vw;
+    background-image: url(${logo});
     background-repeat: no-repeat;
     background-size: 100% 100%;
-    position: fixed;
+    position: relative;
+    left: 1vw;
   `
+  const BackgroundDiv = styled.div`
+    width: 99vw;
+    height: 100vw;
+    display: flex;
+    justify-contents: center;
+
+    
+
+  `
+  const Background = styled.div`
+    width: 80vw;
+    height: 50vw;
+    margin-left: auto;
+    margin-right: auto;
+    background-image: url(${bg});
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+    
+  `;
+  
   const Textbox = styled.div`
     width: 30vh;
     height: 20vh;
@@ -33,11 +60,20 @@ function LandingPage() {
   `
   return (
     <Container>
-        <Logo/>
-            
-        <Textbox>
-         test
-        </Textbox>
+        <Header>
+          <Logo/>
+        </Header>
+        
+          
+        <BackgroundDiv>
+          <Background>
+              <Textbox>
+              </Textbox>
+          </Background>
+
+        </BackgroundDiv>
+         
+          
     </Container>
     
   );
