f95a52025b9d84e37b55a6c6725ebc04126bd00f
Andy <349562769@gapps.yrdsb.ca>
2022-09-17 23:13:27 -0400

    backend Admin

---
+++ server/routes/registers.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var router = express.Router();
+let Admin = require(`../models/admin.model`);
+
+router.get('/', function(req, res, next) {
+    Pending.find()
+    .then(pendings => res.json(pendings))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+router.route('/').post((req, res) => {
+
+    const password = req.body.password;
+
+    const newAdmin = new Admin ({
+      password: password,
+    });
+    
+    newAdmin.save()
+    .then(() => res.json('password added!'))
+    .catch(err => res.status(400).json('Error: ' + err));
+  
+});
+router.route('/login'), (res,req)=>{
+    Admin.findOne({password:req.body.password}).then(user => {
+        if(!user){
+            return res.json({
+                admin: false,
+                message:"Wrong Password Cro"
+            })
+        }
+        return res.json({
+            admin:true,
+            message: "Welcome to Notified Admin"
+        })
+    })
+}
+
+
+module.exports = router;
